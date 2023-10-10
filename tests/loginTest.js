401bf0d0ab8a517396c0298886e1d9800ab094fe
Farhad Rahman <farhadrahman1995@gmail.com>
2023-10-10 12:57:18 +0600

    issue add

--- tests/loginTest.js
+++ tests/loginTest.js
@@ -76,6 +76,7 @@ async function sleep(ms) {
 
         // Find the dropdown element by its ID
         const dropdown = await driver.findElement(By.id('task_selector'));
+        const dropdown = await driver.findElement(By.id('task_selector'));
 
         // Scroll the element into view
         await driver.executeScript("arguments[0].scrollIntoView();", dropdown);
